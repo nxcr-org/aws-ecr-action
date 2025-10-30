6ed08e2718f2b6a283229c36fc7ed23811c15d97
grante-nxcr <102606801+grante-nxcr@users.noreply.github.com>
2025-10-30 10:19:20 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,9 +37,6 @@ function sanitize() {
 }
 
 function aws_configure() {
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  ./aws/install
   export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY
   export AWS_DEFAULT_REGION=$INPUT_REGION
